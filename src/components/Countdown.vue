66f48b88b8cf38816f49739bc50b6bcad5f63bd5
AzureAD\MikkelHornbechNielse <mhn@rosidigital.com>
2022-11-29 14:35:53 +0100

    Added api endpoint - might need to change this

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,4 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+import holidays from 'date-holidays';
+
+const holidayRef = ref(new holidays('DK'));
+const test = holidayRef.value.getHolidays();
+
+onMounted(() => {
+  console.log(test, 'test');
+});
+</script>
 
 <template></template>
 
