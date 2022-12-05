6bf5d988a86f0911779c746f089e323e26d80be0
AzureAD\MikkelHornbechNielse <mhn@rosidigital.com>
2022-12-05 15:59:29 +0100

    Added pwa package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import mkcert from 'vite-plugin-mkcert';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mkcert()],
+  plugins: [vue(), mkcert(), VitePWA({ registerType: 'autoUpdate' })],
 
   server: {
     https: true,
